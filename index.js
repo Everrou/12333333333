f6f26aaeb0284fd623822b78c7bd5eda65d89515
Everrou <126096738+Everrou@users.noreply.github.com>
2023-04-10 20:34:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let otvet=Number(prompt('ИГРАЕМ НА ВЫХОДНЫХ В КС? НАПИШИ 1 ЕСЛИ ДА,НАПИШИ 2 ЕСЛИ НЕТ'))
+switch (otvet) {
+    case 1:{
+        alert('КРАСАВЧИК ЖИ ЕСТЬ')
+break
+    }
+
+    case 2:{
+        alert('ТЫ ЛОХ')
+break
+    }
+
+default: {
+    alert('нет такого')
+}
+}
